a9b95857a124d622c303c43aa37705a50bd8fca9
Hanne <hannehoitzing@gmail.com>
2021-05-09 15:52:10 +0100

    Update alert

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Flink frauddetection example
 
 https://ci.apache.org/projects/flink/flink-docs-release-1.13/docs/try-flink/datastream/
+
+https://ci.apache.org/projects/flink/flink-docs-release-1.12/try-flink/datastream_api.html
--- src/main/java/spendreport/FraudDetector.java
+++ src/main/java/spendreport/FraudDetector.java
@@ -18,21 +18,71 @@
 
 package spendreport;
 
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.walkthrough.common.entity.Alert;
 import org.apache.flink.walkthrough.common.entity.Transaction;
 
+import java.sql.Types;
+
 /**
  * Skeleton code for implementing a fraud detector.
+ * The KeyedProcessFunction provides control over both state and time. (if want to access keyed state and timers
+ * then it has to be applied to a keyed stream, e.g. stream.keyBy(..).KeyProcessFunction()).
+ * Access to state is via RuntimeContext.
+ *
+ * Every call to processElement(...) gets a Context object which gives access to
+ * - event timestamp
+ * - TimerService (used to register callbacks for future event- or process-time instants.
+ *
+ * For event-time timers.
+ * onTime() is called when the current watermark is advanced up to or beyond the timestamp of the timer.
+ *
  */
 public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
 
 	private static final long serialVersionUID = 1L;
+	private transient ValueState<Boolean> flagState;
+	private transient ValueState<Long> timerState;
 
 	private static final double SMALL_AMOUNT = 1.00;
 	private static final double LARGE_AMOUNT = 500.00;
 	private static final long ONE_MINUTE = 60 * 1000;
+
+	@Override
+	public void open(Configuration parameters) {
+		ValueStateDescriptor<Boolean> flagDescriptor = new ValueStateDescriptor<>("flag", Boolean.class);
+		flagState = getRuntimeContext().getState(flagDescriptor);
+
+		ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>("timer-state", Long.class);
+		timerState = getRuntimeContext().getState(timerDescriptor);
+	}
+
+
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+		// remove flag after 1 minute
+		timerState.clear();
+		flagState.clear();
+	}
+
+
+
+	private void cleanUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+
+		// clean up all state
+		timerState.clear();
+		flagState.clear();
+	}
+
+
 
 	@Override
 	public void processElement(
@@ -40,9 +90,28 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 			Context context,
 			Collector<Alert> collector) throws Exception {
 
-		Alert alert = new Alert();
-		alert.setId(transaction.getAccountId());
+		// current state for current key
+		Boolean lastTransactionWasSmall = flagState.value();
 
-		collector.collect(alert);
+		if (lastTransactionWasSmall != null) {
+			if (transaction.getAmount() > LARGE_AMOUNT) {
+				Alert alert = new Alert();
+				alert.setId(transaction.getAccountId());
+				collector.collect(alert);
+			}
+
+			cleanUp(context);  // reset state
+		}
+
+		if (transaction.getAmount() < SMALL_AMOUNT) {
+			flagState.update(true);
+
+			// set the timer and timer state
+			long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+			context.timerService().registerProcessingTimeTimer(timer);
+			timerState.update(timer);
+		}
+
+
 	}
 }
